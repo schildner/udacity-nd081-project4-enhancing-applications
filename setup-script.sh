8b7c43a9c95b64e40239b3e8930fb49bffe07d6d
Eduard Schildner <eduard.schildner@gmail.com>
2023-06-30 20:02:04 +0200

    Initial commit for Deploy_to_VMSS branchk

--- setup-script.sh
+++ setup-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables
-resourceGroup="acdnd-c4-project"
-location="westus"
+resourceGroup="nd081-project-04-azure-performance-rg"
+location="eastus"
 osType="UbuntuLTS"
 vmssName="udacity-vmss"
 adminName="udacityadmin"
